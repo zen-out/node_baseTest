ceed5b21bc7e45ffdbbc8f06dbf45f679d22db68
Lesley Cheung <lesley.yc@gmail.com>
2022-03-24 14:02:46 +0800

    updated

---
+++ dist/list.js
@@ -0,0 +1,27 @@
+function list() {
+    let string = `
+  
+  HAS,
+  HASHELPER,
+  STARTSWITH,
+  STARTSWITHHELPER,
+  ENDSWITH,
+  ENDSWITHHELPER,
+  ISLESS,
+  TRUNCATE,
+  WRAPHTML,
+  ISLESSHELPER,
+  COUNT,
+  COUNTHELPER,
+  ATLEAST,
+  ATLEASTHELPER,
+  DOESNOTHAVE,
+  DOESNOTHAVEHELPER,
+  T,
+  F,
+  ISTYPEOF,
+  ISTYPEOFHELPER`
+    console.log(string)
+    return string;
+}
+module.exports = { list }
--- playground.js
+++ playground.js
@@ -1,5 +1,3 @@
 const t = require("./index.js")
 
-let getThis = t.ISTYPEOFHELPER([3], "array")
-
-console.log("🚀 ~ file: playground.js ~ line 3 ~ getThis", getThis)
+t.list()
